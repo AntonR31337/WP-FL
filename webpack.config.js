79fd2a0a9f1d812c9a6fcf30b550464eefe13a2a
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-01-02 09:24:40 +0300

    configurated webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: 'main.[contenthash].js',
+        path: resolve(__dirname, "build")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/1,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, 'src', 'index.html')
+        })
+    ]
+}
