1160059a9a4bfec4986058ba2c03c2185ced7ae3
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-01-05 09:29:33 +0300

    fixed some problems with building

---
+++ src/items.js
@@ -0,0 +1,23 @@
+import image1 from "./media/images/1.jpg"
+import image2 from "./media/images/2.jpg"
+import image3 from "./media/images/3.jpg"
+import image4 from "./media/images/4.gif"
+
+export default [
+    {
+        resourse: image1,
+        title: 'Мельница'
+    },
+    {
+        resourse: image2,
+        title: 'Горы'
+    },
+    {
+        resourse: image3,
+        title: 'Деревья'
+    },
+    {
+        resourse: image4,
+        title: 'Горы с анимацией'
+    },
+]
