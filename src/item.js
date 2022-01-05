aa091ac52561933cd23d9906e208247478170fad
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-01-05 13:46:51 +0300

    fixed a bug with a function

--- src/item.js
+++ src/item.js
@@ -2,17 +2,28 @@ export function drawGalleryItem(item) {
     const itemElement = document.createElement('div')
     itemElement.classList = "gallery-item"
 
-    const resourceWrapElement = document.childElement('div')
+    const resourceWrapElement = document.createElement('div')
+    resourceWrapElement.classList = 'gallery-item__resource'
 
-    const imgElement = document.createElement('img')
-    imgElement.classList = "gallery-item__image"
-    imgElement.src = item.resource
+    if (item.type === "image") {
+        const imgElement = document.createElement('img')
+        imgElement.classList = "gallery-item__image"
+        imgElement.src = item.resource
+
+        resourceWrapElement.appendChild(imgElement)
+    } else if (item.type === "audio") {
+        const audioElement = document.createElement('audio')
+        audioElement.classList = "gallery-item__audio"
+        audioElement.src = item.resource
+
+        resourceWrapElement.appendChild(audioElement)
+    }
 
     const titleElement = document.createElement('span')
     titleElement.classList = "gallery-item__title"
     titleElement.textContent = item.title
 
-    itemElement.appendChild(imgElement)
+    itemElement.appendChild(resourceWrapElement)
     itemElement.appendChild(titleElement)
 
     return itemElement
